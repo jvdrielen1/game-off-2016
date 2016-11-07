18e85141baa23841f7501bd28ce603c1a83e9668
Julian <79701@stu.glr.nl>
2016-11-07 17:02:00 +0100

    Added test scene and played with some trails

---
+++ Hackulous/Assets/script/SphereMove.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SphereMove : MonoBehaviour {
+
+	public float speed = 5;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float horizontal = Input.GetAxis ("Horizontal");
+		float vertical = Input.GetAxis ("Vertical");
+
+		transform.position = new Vector3 (transform.position.x + horizontal * Time.deltaTime * speed, transform.position.y + vertical * Time.deltaTime * speed, 0);
+	}
+}
